216bdd8bdc3291e15c5f6a81c185abf714c925c2
nartc <ctch5@mail.umsl.edu>
2017-09-28 22:08:00 -0500

    Added Google OAuth Strategy

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require('passport');
 
 //Express Variable
 const app = express();
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
+
 
 //Index route
 app.get('/', (req, res) => {
     res.send('It works');
 });
+
+//Use Routes
+app.use('/auth', auth);
 
 //PORT
 const port = process.env.PORT || 3000;
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://chau:chau@ds155674.mlab.com:55674/storybooks-ct-dev',
+    googleClientID: '260632591841-rlrn2jqt17u5ssij47meaqpv395saukt.apps.googleusercontent.com',
+    googleClientSecret: 'ZaOKpTMUMVQaPzJq8dQ2A77t'
+}
